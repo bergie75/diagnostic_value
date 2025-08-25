7c0b587b6a3fb6d2dbc40de489d322b339940bba
eb <89044765+bergie75@users.noreply.github.com>
2025-08-25 18:36:11 -0400

    Retooled for faster Monte-Carlo

--- mc_computations.py
+++ mc_computations.py
@@ -1,24 +1,19 @@
 import numpy as np
+import time
+import matplotlib.pyplot as plt
 from parameters import *
 
 rng = np.random.default_rng()  # the generator used for our experiments, can set a seed for consistency
 
 def h_helper(f):
-    h=-drug_costs  # holds values of treatments, begin by subtracting costs
-    for i in range(0, num_treatments):
-        for j in range(0, num_pathogens):
-            r_ij = resistance[i,j]
-            h[i] += f[j]*(r_ij*(cost_per_qaly*qaly_if_res-cost_res)+(1-r_ij)*cost_per_qaly*qaly_if_susc)
-    
-    return np.array(h)
+    return -drug_costs + np.matmul(res_benefit_matrix, f)  # holds values of treatments, begin by subtracting costs
 
 # uses the above helper
 true_scores = h_helper(f_true)
 
 # returns the argument of the best empirical diagnosis given that the believed distribution is f_realized
 def argmax_helper(f_realized):
-    treatment_scores = h_helper(f_realized)
-    return np.argmax(treatment_scores)
+    return np.argmax(h_helper(f_realized))
 
 def objective_function(p_test):
     prior_alpha = m*f_0  # retrieve information on prior
@@ -29,9 +24,9 @@ def objective_function(p_test):
         # one run of testing p_test of our patients to improve empirical diagnoses
         other_diagnostics = rng.multinomial((num_patients-1)*p_test, f_true)
         prob_estimates = np.zeros(num_treatments)  # will hold probability of treatment being best
+        # update prior based on the results of our randomly generated measurements
+        posterior_alpha = prior_alpha + other_diagnostics
         for _ in range(0, prior_update_realizations):
-            # update prior based on the results of our randomly generated measurements
-            posterior_alpha = prior_alpha + other_diagnostics
             f_realized = rng.dirichlet(posterior_alpha)
             prob_estimates[argmax_helper(f_realized)] += 1
         
@@ -40,10 +35,12 @@ def objective_function(p_test):
     
     emp_net_benefit_estimate = emp_net_benefit_estimate/diagnostic_realizations
     testing_net_benefit = np.dot(f_true, optimal_net_benefits)-cost_test
-
     obj_value = (1-(1-p_ignore)*p_test)*emp_net_benefit_estimate + (1-p_ignore)*p_test*testing_net_benefit - p_test*p_ignore*cost_test
 
     return obj_value
 
 if __name__ == "__main__":
-    print(objective_function(0.5))
+    p_test_vals = np.array(range(0, num_patients+1))/num_patients
+    obj_fun_vals = np.array([objective_function(p_test) for p_test in p_test_vals])
+    plt.plot(p_test_vals, obj_fun_vals)
+    plt.show()
--- parameters.py
+++ parameters.py
@@ -8,33 +8,33 @@ num_treatments = 2  # number of different drugs available
 num_pathogens = 2 # number of disease-causing agents to consider
 
 # ____________________________________________________________________________
-# MCMC control parameters
+# MONTE CARLO CONTROL
 # ____________________________________________________________________________
-diagnostic_realizations = 10**3
-prior_update_realizations = 10**3
+diagnostic_realizations = 10**2
+prior_update_realizations = 10**2
 
 
 # ____________________________________________________________________________
 # BEHAVIORAL PARAMETERS
 # ____________________________________________________________________________
-p_ignore = 0.01 # probability that a test is ignored after being ordered
+p_ignore = 0.0 # probability that a test is ignored after being ordered
 
 # ____________________________________________________________________________
 # DISTRIBUTIONS AND PRIORS
 # ____________________________________________________________________________
-f_true = np.array([0.3,0.7])  # true species breakdown
+f_true = np.array([0.9,0.1])  # true species breakdown
 m = 10
-f_0 = np.array([0.2, 0.8])
+f_0 = np.array([0.5, 0.5])
 
 # ____________________________________________________________________________
 # COSTS AND BENEFITS
 # ____________________________________________________________________________
-drug_costs = np.array([10,20])  # cost in dollars of drugs
-cost_per_qaly = 1  # conversion factor between expected outcomes and dollars
-qaly_if_susc = 3  # benefit to patient in qaly, could adjust to months
-qaly_if_res = 0  # set to zero for now, could increase if there is residual symptom relief followed by resurgence
-cost_res = 5  # cost of follow-on treatment due to resistance
-cost_test = 2 # cost of giving patient a diagnostic
+drug_costs = np.array([0.78,0.31])  # cost in dollars of drugs
+cost_per_qaly = 53.91  # conversion factor between expected outcomes and dollars
+qaly_if_susc = np.array([0.806,0.807]) # benefit to patient in qaly, could adjust to months
+qaly_if_res = np.array([0.68,0.68])  # set to zero for now, could increase if there is residual symptom relief followed by resurgence
+cost_res = np.array([28.98,33.79])  # cost of follow-on treatment due to resistance
+cost_test = 0.15 # cost of giving patient a diagnostic
 
 # ____________________________________________________________________________
 # PATHOGEN/DRUG DYNAMICS
@@ -42,23 +42,23 @@ cost_test = 2 # cost of giving patient a diagnostic
 
 # each row represents a drug, each column a bug. Entry is resistance prob. of bug to drug
 # note that these do not need to sum to one along rows or columns
-resistance = np.array([[0.1, 0.6],[0.55, 0.05]])
+resistance = np.array([[140/189, 39/57],[183/230, 48/60]])
 
+# we combine this with previous variables to aid a function in the mc_computations file
+res_benefit_matrix = np.zeros((num_treatments, num_pathogens))
+for i in range(0, num_treatments):
+      for j in range(0, num_pathogens):
+            r_ij = resistance[i,j]
+            res_benefit_matrix[i,j] = resistance[i,j]*(r_ij*(cost_per_qaly*qaly_if_res[i]-cost_res[j])+(1-r_ij)*cost_per_qaly*qaly_if_susc[i])
+            
 # ____________________________________________________________________________
 # OPTIMAL NET BENEFIT GIVEN TESTING
 # ____________________________________________________________________________
-optimal_net_benefits = []
+optimal_net_benefits_list = []
 
-# loops to populate optimal benefits
+# loop to populate optimal benefits
 for j in range(0, num_pathogens):
-    # variable to hold current best choice
-    max_benefit = -np.inf
-    
-    for i in range(0, num_treatments):
-            r_ij = resistance[i,j]
-            benefit_under_i = r_ij*(cost_per_qaly*qaly_if_res-cost_res)+(1-r_ij)*(cost_per_qaly*qaly_if_susc)-drug_costs[i]
-            if benefit_under_i > max_benefit:
-                  max_benefit = benefit_under_i
-    
-    # after defining all possible benefits, select the best drug for treating the given pathogen
-    optimal_net_benefits.append(max_benefit)
+    benefits = res_benefit_matrix[:,j]
+    optimal_net_benefits_list.append(np.max(benefits))
+
+optimal_net_benefits = np.array(optimal_net_benefits_list)
