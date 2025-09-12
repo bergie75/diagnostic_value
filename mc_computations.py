323014659229d6ec985c7d7035bac8862277a1a8
eb <89044765+bergie75@users.noreply.github.com>
2025-09-12 16:39:21 -0400

    Added function to run experiments, changing any number of variables from standard values

--- mc_computations.py
+++ mc_computations.py
@@ -1,8 +1,11 @@
+# math and user-defined
 import numpy as np
 from scipy.special import betainc
-import os
 import matplotlib.pyplot as plt
 from example_with_maximum import exported_parameters, par_indices
+# logistical
+import os
+import itertools
 
 # the generator used for our experiments, can set a seed for consistency
 rng = np.random.default_rng()
@@ -91,33 +94,84 @@ def objective_function_for_2_by_2(p_test, parameters=exported_parameters, print_
 
     return obj_value
 
-if __name__ == "__main__":
+def find_critical_cost(parameters=exported_parameters):
+    f_true = parameters[par_indices["f_true"]]
+    res_benefit_matrix = res_ben_matrix(parameters=parameters)
+    optimal_net_benefits = opt_net_benefits(res_benefit_matrix, parameters=exported_parameters)
+    
+    return np.dot(f_true, optimal_net_benefits)
+
+def run_experiment(experiment_name, vars_changed, changes_to_try, local_params=exported_parameters):
+    # preliminaries to save the results
     cwd = os.getcwd()
-    filename = "examining_cost_dependence"
-    save_to = os.path.join(cwd,"diagnostic_value","longer_runs",filename)
+    save_to = os.path.join(cwd,"diagnostic_value","longer_runs", experiment_name)
 
     if not os.path.exists(save_to):
         os.makedirs(save_to)
 
+    # generate testing frequencies
     num_patients = exported_parameters[par_indices["num_patients"]]  # a constant from our configuration file
     p_test_vals = np.array(range(0, num_patients+1))/num_patients
-    test_cost_vals = np.linspace(-2,0,11)  # we will examine these costs to see how the net benefit curve changes
-    local_params = exported_parameters
-    local_params[par_indices["f_true"]] = [0.9, 0.1]
-    dis1_prev = local_params[par_indices["f_true"]][0]
 
-    for x in test_cost_vals:
-        print(f"Working on test cost=${x}")
-        local_params[par_indices["cost_test"]] = x
+    # generate tests
+    combinations = list(itertools.product(*changes_to_try))
+    
+    for combo in combinations:
+        # prepare to save results to specific file and change local variables for experiment
+        print(f"Working on combination={combo}")
+        label = ""
+        
+        for i,var_name in enumerate(vars_changed):
+            label += f"{var_name}_{combo[i]}_"
+            local_params[par_indices[var_name]] = combo[i]
+        
+        label = label[:-1]  # strip trailing underscore
+
+        # carry out experiment and add results to plot
         obj_fun_vals = np.array([objective_function_for_2_by_2(p_test, parameters=local_params) for p_test in p_test_vals])
         plt.plot(p_test_vals, obj_fun_vals)
-        plt.title("Tradeoffs between public and private value of diagnostics")
-        plt.xlabel("Net benefit ($)")
-        plt.ylabel("Ratio of public to private net benefit")
-        np.save(os.path.join(save_to, f"dis1_prev_{dis1_prev}_test_cost_{x}"), obj_fun_vals)  # save results to view later
+        np.save(os.path.join(save_to, label), obj_fun_vals)  # save results to view later
 
-    plt.legend([f"${x}" for x in test_cost_vals])
+    plt.title("Tradeoffs between public and private value of diagnostics")
+    plt.xlabel("Net benefit ($)")
+    plt.ylabel("Sum of public and private net benefit")
+    plt.legend([f"{combo}" for combo in combinations])
     plt.show()
+
+if __name__ == "__main__":
+    vars_changed = ["cost_test", "p_ignore"]
+    changes_to_try = [np.linspace(4,6,11), [1]]
+    run_experiment("ignoring_tests", vars_changed, changes_to_try)
+    # cwd = os.getcwd()
+    # filename = "ignoring_tests"
+    # save_to = os.path.join(cwd,"diagnostic_value","longer_runs", filename)
+
+    # if not os.path.exists(save_to):
+    #     os.makedirs(save_to)
+
+    # num_patients = exported_parameters[par_indices["num_patients"]]  # a constant from our configuration file
+    # p_test_vals = np.array(range(0, num_patients+1))/num_patients
+    # test_cost_vals = np.linspace(2,4,11)  # we will examine these costs to see how the net benefit curve changes
+    # local_params = exported_parameters
+    # ignore_probs = [1]
+    # # worse_res_factor = 1
+    # # local_params[par_indices["qaly_if_res"]] = local_params[par_indices["qaly_if_res"]]/worse_res_factor
+    # # local_params[par_indices["cost_test"]] = find_critical_cost(local_params)
+    
+    # for cost_test in test_cost_vals:
+    #     for ignore_prob in ignore_probs:
+    #         print(f"Working on test cost=${cost_test}")
+    #         local_params[par_indices["p_ignore"]] = ignore_prob
+    #         local_params[par_indices["cost_test"]] = cost_test
+    #         obj_fun_vals = np.array([objective_function_for_2_by_2(p_test, parameters=local_params) for p_test in p_test_vals])
+    #         plt.plot(p_test_vals, obj_fun_vals)
+    #         plt.title("Tradeoffs between public and private value of diagnostics")
+    #         plt.xlabel("Net benefit ($)")
+    #         plt.ylabel("Sum of public and private net benefit")
+    #         np.save(os.path.join(save_to, f"test_cost_{cost_test}_ignore_prob_{ignore_prob}"), obj_fun_vals)  # save results to view later
+
+    # plt.legend([f"${cost_test:.2f}" for cost_test in test_cost_vals])
+    # plt.show()
 
     # p_test_vals = np.array(range(0, num_patients+1))/num_patients
     # evsi_vals = np.array([evsi(p_test) for p_test in p_test_vals])
