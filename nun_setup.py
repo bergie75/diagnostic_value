72c243aa7ce59480c00662f1e0e68ba9c61f9a75
eb <89044765+bergie75@users.noreply.github.com>
2026-01-09 15:36:37 -0500

    extra

--- nun_setup.py
+++ nun_setup.py
@@ -60,6 +60,51 @@ def net_testing_value(bias_value, local_params=exported_parameters):
 
     return net_testing_value
 
+def find_tradeoff_points(base_price, local_params=exported_parameters):
+    # unpack important parameters
+    f_true = local_params["f_true"]
+    cost_per_qaly = local_params["cost_per_qaly"]
+    qaly_if_susc = local_params["qaly_if_susc"]
+    qaly_if_res = local_params["qaly_if_res"]
+    drug_costs = local_params["drug_costs"]
+    cost_res = local_params["cost_res"]
+    f_0 = local_params["f_0"]
+    
+
+    have_zero_diag_one = (drug_costs[1]-drug_costs[0]+cost_res[0]+cost_per_qaly*qaly_if_susc[0]-cost_per_qaly*qaly_if_res[0])
+    have_one_diag_zero = (drug_costs[0]-drug_costs[1]+cost_res[1]+cost_per_qaly*qaly_if_susc[1]-cost_per_qaly*qaly_if_res[1])
+    
+    candidates = []
+
+    # find first tradeoff point
+    a_0 = -base_price
+    a_1 = 2*have_zero_diag_one*f_true[0]+2*have_one_diag_zero*f_0[0]
+    a_2 = -4*f_true[0]*f_0[0]*(have_one_diag_zero+have_zero_diag_one)
+
+    z_1 = (-a_1-np.sqrt(a_1**2-4*a_0*a_2))/(2*a_2)
+    z_2 = (-a_1+np.sqrt(a_1**2-4*a_0*a_2))/(2*a_2)
+
+    if 0<=z_1<=0.5:
+        candidates.append(z_1)
+    if 0<=z_2<=0.5:
+        candidates.append(z_2)
+
+    # find second tradeoff point
+    b_0 = -base_price
+    b_1 = 2*have_zero_diag_one*f_0[1]+2*have_one_diag_zero*f_true[1]
+    b_2 = -4*f_true[1]*f_0[1]*(have_one_diag_zero+have_zero_diag_one)
+
+    w_1 = 1-((-b_1-np.sqrt(b_1**2-4*b_0*b_2))/(2*b_2))
+    w_2 = 1-((-b_1+np.sqrt(b_1**2-4*b_0*b_2))/(2*b_2))
+
+    if 0.5<=w_1<=1:
+        candidates.append(w_1)
+    if 0.5<=w_2<=1:
+        candidates.append(w_2)
+    
+    candidates.sort()
+    return candidates
+
 def empirical_value(bias_value, local_params=exported_parameters):
     # unpack important parameters
     f_true = local_params["f_true"]
@@ -106,7 +151,7 @@ def testing_value(bias_value, local_params=exported_parameters):
     return testing_value
 
 # density function must take a bias value and return the probability density function at that value
-def value_calculations(base_price, density_function, local_params=exported_parameters):
+def value_calculations(base_price, density_function, local_params=exported_parameters, biased_update=False):
     # separate dictionaries to pass as arguments when computing average values
     non_testing_params = deepcopy(local_params)
     testing_params = deepcopy(local_params)
@@ -118,37 +163,46 @@ def value_calculations(base_price, density_function, local_params=exported_param
     num_patients = testing_params["num_patients"]
 
     # first, find the bias values that bracket the region where testing should occur
-    t_vals = np.linspace(0,1,1000)
-    net_testing_vals = [net_testing_value(t, local_params=local_params) for t in t_vals]
+    # t_vals = np.linspace(0,1,1000)
+    # net_testing_vals = [net_testing_value(t, local_params=local_params) for t in t_vals]
 
-    # find crossing values
-    for i,test_val in enumerate(net_testing_vals):
-        if test_val >= base_price:
-            first_index = i
-            break
+    # # find crossing values
+    # for i,test_val in enumerate(net_testing_vals):
+    #     if test_val >= base_price:
+    #         first_index = i
+    #         break
     
-    for i in range(first_index, len(net_testing_vals)):
-        test_val = net_testing_vals[i]
-        if test_val < base_price or i==len(net_testing_vals)-1:
-            last_index = i
-            break
-
+    # for i in range(first_index+1, len(net_testing_vals)):
+    #     test_val = net_testing_vals[i]
+    #     if test_val <= base_price or i==len(net_testing_vals)-1:
+    #         last_index = i
+    #         break
+    
+    lower_crossing, higher_crossing = find_tradeoff_points(base_price)
+    
     # compute the fraction of patients who will undergo diagnostic testing
-    testing_frac = quad(lambda t: density_function(t), t_vals[first_index], t_vals[last_index])[0]  # second entry is error
+    testing_frac = quad(lambda t: density_function(t), lower_crossing, higher_crossing)[0]  # second entry is error
 
     # compute the value to testers
     testing_integrand = lambda t: density_function(t)*testing_value(t, local_params=testing_params)
-    test_value = quad(testing_integrand, t_vals[first_index], t_vals[last_index])[0]-base_price*testing_frac
+    test_value = quad(testing_integrand, lower_crossing, higher_crossing)[0]-base_price*testing_frac
     
     # update with measurement results (assume to be average, for simplicity)
     updated_m = m + num_patients*testing_frac
-    updated_f_0 = (m*f_0 + num_patients*testing_frac*f_true)/updated_m
+    if not biased_update:
+        updated_f_0 = (m*f_0 + num_patients*testing_frac*f_true)/updated_m
+    else:
+        bias_integrand = lambda t: num_patients*density_function(t)*f_true[0]*(bias_array(t)[0])
+        biased_result_0 = quad(bias_integrand, lower_crossing, higher_crossing)[0]
+        biased_result_1 = num_patients*testing_frac - biased_result_0
+        biased_results = np.array([biased_result_0, biased_result_1])
+        updated_f_0 = (m*f_0 + biased_results)/updated_m
     
     # use the results of the testing to update physician beliefs for non-testers
     non_testing_params["m"] = updated_m
     non_testing_params["f_0"] = updated_f_0
     non_testing_integrand = lambda t: density_function(t)*empirical_value(t, local_params=non_testing_params)
-    non_test_value = quad(non_testing_integrand, 0, t_vals[first_index])[0] + quad(non_testing_integrand, t_vals[last_index], 1)[0]
+    non_test_value = quad(non_testing_integrand, 0, lower_crossing)[0] + quad(non_testing_integrand, higher_crossing, 1)[0]
 
     return test_value, non_test_value, testing_frac
 
@@ -165,13 +219,21 @@ if __name__ == "__main__":
         if test_val >= base_price:
             first_index = i
             break
-    
+
     for i in range(first_index, len(net_testing_vals)):
         test_val = net_testing_vals[i]
         if test_val <= base_price:
             last_index = i
             break
 
+    print(f"[{t_vals[first_index]}, {t_vals[last_index]}]")
+    first_crossing, last_crossing = find_tradeoff_points(base_price)
+    print(net_testing_value(t_vals[first_index])-base_price)
+    print(net_testing_value(t_vals[last_index])-base_price)
+    print(f"\n[{first_crossing}, {last_crossing}]")
+    print(net_testing_value(first_crossing)-base_price)
+    print(net_testing_value(last_crossing)-base_price)
+    
     # two possible risk distributions
     uniform = lambda t: 1
 
@@ -198,23 +260,22 @@ if __name__ == "__main__":
     plt.xticks([0,t_vals[first_index], t_vals[last_index], 1], labels=[0,"L", "U", 1])
     plt.figtext(
     0.43, 0.07, f"Privately chooses testing", wrap=True, horizontalalignment='center', fontsize=8)
-    print(last_index)
     plt.show()
 
     pop_density = lambda t: edge_cases(t, edge_width=0.3)
     density_values = [pop_density(t) for t in t_vals]
-    plt.plot(t_vals, density_values)
-    plt.xlim(0,1)
-    plt.xticks([0,t_vals[first_index], t_vals[last_index], 1], labels=[0,"L", "U", 1])
-    plt.xlabel("Patient risk for disease one")
-    plt.ylabel("Number of patients")
-    plt.yticks(())
-    plt.vlines(t_vals[first_index], 0, density_values[first_index], color="black", linestyles="dashed")
-    plt.vlines(t_vals[last_index], 0, density_values[last_index], color="black", linestyles="dashed")
-    plt.fill_between(t_vals[first_index:last_index+1], density_values[first_index:last_index+1], alpha=0.3)
-    plt.figtext(0.465, 0.47, f"Population \ndemand for testing", wrap=True, horizontalalignment='center', fontsize=9)
-    plt.ylim(0,1.1*max(density_values))
-    plt.show()
+    # plt.plot(t_vals, density_values)
+    # plt.xlim(0,1)
+    # plt.xticks([0,t_vals[first_index], t_vals[last_index], 1], labels=[0,"L", "U", 1])
+    # plt.xlabel("Patient risk for disease one")
+    # plt.ylabel("Number of patients")
+    # plt.yticks(())
+    # plt.vlines(t_vals[first_index], 0, density_values[first_index], color="black", linestyles="dashed")
+    # plt.vlines(t_vals[last_index], 0, density_values[last_index], color="black", linestyles="dashed")
+    # plt.fill_between(t_vals[first_index:last_index+1], density_values[first_index:last_index+1], alpha=0.3)
+    # plt.figtext(0.465, 0.47, f"Population \ndemand for testing", wrap=True, horizontalalignment='center', fontsize=9)
+    # plt.ylim(0,1.1*max(density_values))
+    # plt.show()
 
     test_value, non_test_value, testing_frac = value_calculations(base_price, pop_density)
     bar_locs = [-0.2, 0.2]
@@ -224,4 +285,9 @@ if __name__ == "__main__":
     plt.bar(bar_locs, [test_value, non_test_value], width=0.2)
     plt.show()
 
-
+    test_value, non_test_value, testing_frac = value_calculations(base_price, pop_density, biased_update=True)
+    
+    plt.xticks(bar_locs, labels=["Value to testers", "Value to non-testers"])
+    plt.ylabel("Value per patient ($)")
+    plt.bar(bar_locs, [test_value, non_test_value], width=0.2)
+    plt.show()
--- plotting_pipelines.py
+++ plotting_pipelines.py
@@ -1,9 +1,58 @@
 from mc_computations import *
 from philipson_setup import *
+from nun_setup import value_calculations, net_testing_value, find_tradeoff_points
 from example_with_maximum import exported_parameters
 import os
 import matplotlib.pyplot as plt
 from copy import deepcopy
+
+def curly_brace(axes, start, end, height_one, height_two, parabola_width, exp_width, color="black"):
+    #find points to connect to brace
+    start_x, start_y = start
+    end_x, end_y = end
+    t_vals=np.linspace(0,1,1000)
+    
+    # plot first hook in braces
+    start_connector_x = start_x + parabola_width
+    start_connector_y = start_y + height_one
+    x_vals = [(1-t)*start_x+t*start_connector_x for t in t_vals]
+    a=start_connector_y/parabola_width**2
+    y_vals = [a*(x-start_x)*(start_x+2*parabola_width-x) for x in x_vals]
+    axes.plot(color=color)
+    axes.plot(x_vals, y_vals, color=color)
+    
+    # plot straight segment
+    start_of_sharp = start_connector_x+(end_x-start_x-2*parabola_width-2*exp_width)/2
+    x_vals = [(1-t)*start_connector_x+t*start_of_sharp for t in t_vals]
+    y_vals = [start_connector_y for x in x_vals]
+    axes.plot(x_vals, y_vals, color=color)
+
+    # half sharp segment
+    x_vals = [(1-t)*start_of_sharp+t*(start_of_sharp+exp_width) for t in t_vals]
+    a=np.log((height_one+height_two+start_y)/(height_one+start_y))/exp_width
+    y_vals = [(height_one+start_y)*np.exp(a*(x-start_of_sharp)) for x in x_vals]
+    axes.plot(x_vals, y_vals, color=color)
+
+    # other half
+    x_vals = [(1-t)*(start_of_sharp+exp_width)+t*(start_of_sharp+2*exp_width) for t in t_vals]
+    x_vals = list(reversed(x_vals))
+    a=np.log((height_one+start_y)/(height_one+height_two+start_y))/exp_width
+    y_vals = [(height_one+height_two+start_y)*np.exp(a*(x-start_of_sharp-exp_width)) for x in x_vals]
+    axes.plot(x_vals, y_vals, color=color)
+    
+    # other straight
+    start_of_sharp = start_connector_x+(end_x-start_x-2*parabola_width-2*exp_width)/2
+    end_second_straight = start_of_sharp+2*exp_width + (end_x-start_x-2*parabola_width-2*exp_width)/2
+    x_vals = [(1-t)*(start_of_sharp+2*exp_width)+t*end_second_straight for t in t_vals]
+    y_vals = [start_connector_y for x in x_vals]
+    axes.plot(x_vals, y_vals, color=color)
+
+    # final parabola
+    x_vals = [(1-t)*end_second_straight+t*end_x for t in t_vals]
+    a=start_connector_y/parabola_width**2
+    y_vals = [a*(x-end_x)*(end_x-2*parabola_width-x) for x in x_vals]
+    axes.plot(color=color)
+    axes.plot(x_vals, y_vals, color=color)
 
 def one_frequency_temporal_plots(delta_list, local_parameters=deepcopy(exported_parameters), plot_labels=None):
     local_parameters["resistance"] = np.array([[140/189, 1],[1, 39/57]])
@@ -76,7 +125,6 @@ def comparative_philipson(change_var, change_vals, mode=None, base_price=36, max
     # prepare canvas for graphs
     _, axs = plt.subplots(2,2)
 
-    
     axs[0][0].set_xlabel("subsidy per patient ($)")
     axs[0][0].set_ylabel("value per patient ($)")
     axs[1][0].set_xlabel("subsidy per patient ($)")
@@ -161,6 +209,176 @@ def comparative_philipson(change_var, change_vals, mode=None, base_price=36, max
     axs[0][0].legend()
     axs[1][0].legend()
     axs[1][1].legend()
+    plt.tight_layout()
+    plt.show()
+
+def bar_philipson(base_price, change_var, change_vals, mode=None, 
+                          use_resampling=False, local_params=deepcopy(exported_parameters),
+                          other_changes=None, alt_plot=None):
+    # change previous parameters
+    base_value = local_params[change_var]
+
+    if other_changes is not None:
+        for key in other_changes.keys():
+            local_params[key] = other_changes[key]
+
+    # prepare canvas for graphs
+    _, axs = plt.subplots(2,1)
+  
+    axs[0].set_xticks([-0.5, 0.5], labels=["value to non-testers", "value to testers"])
+    axs[0].set_ylabel("value per patient ($)")
+    axs[1].set_ylabel("density of patients")
+    axs[1].set_xlabel("cost of empirical treatment")
+
+    distribution_parameters = []
+    label_list = []
+    # divvy up the range for both non testing and testing between the different scenarios, some logic based on even or odd cases
+    num_changes = len(change_vals)
+    separation = 0.4/num_changes
+    if num_changes % 2 == 1:
+        offset = 0
+    else:
+        offset = 0.5
+    
+    lattice = [i+offset for i in range(-int(num_changes//2), 0)]
+    if num_changes % 2 == 1:
+        lattice.append(0)
+    lattice.extend([i-offset for i in range(1, 1+int(num_changes//2))])
+    bar_locations = -0.5+separation*np.array(lattice)
+    
+    for i,change_value in enumerate(change_vals):
+        # set up current experiment
+        if mode == "*":
+            local_params[change_var] = base_value*change_value
+        elif mode == "+":
+            local_params[change_var] = base_value+change_value
+        else:
+            local_params[change_var] = change_value
+        
+        pub_val, priv_val, _ = philipson_objective_value(base_price, local_params=deepcopy(local_params), use_resampling=use_resampling)
+        
+        #axs[0][0].plot(subsidies, philipson_values, label="total")
+        if mode == "*":
+            label = f"{change_value}x baseline {change_var}"
+        elif mode == "+":
+            label = f"baseline {change_var}+{change_value}"
+        else:
+            label = f"{change_var}={change_value}"
+
+        label_list.append(label)
+        
+        axs[0].bar([bar_locations[i], 1+bar_locations[i]], [pub_val, priv_val], width=separation, label=label_list[i])
+
+        # prepare a Philipson plot for the number of patients testing
+        res_cost_mean = local_params["cost_res"]
+        res_cost_variance = local_params["variance_res"]
+
+        mu, sigma_squared = final_mean_and_variance(res_cost_mean, res_cost_variance, local_params=deepcopy(local_params))
+        sigma = np.sqrt(sigma_squared)
+        distribution_parameters.append([mu, sigma])
+        
+    xmax = max([x[0]+3*x[1] for x in distribution_parameters])
+    xmin = min([x[0]-3*x[1] for x in distribution_parameters])
+    full_domain = np.linspace(xmin, xmax, 1000)
+    
+    for i,dist_pars in enumerate(distribution_parameters):
+        mu, sigma = dist_pars
+        gaussian = lambda x: 1/np.sqrt(2*np.pi*sigma**2)*np.exp(-(x-mu)**2/(2*sigma**2))
+        right_of_price = np.linspace(base_price, xmax, 1000)
+        
+        axs[1].plot(full_domain, gaussian(full_domain), label=label_list[i])
+        axs[1].fill_between(right_of_price, gaussian(right_of_price), alpha=0.3)
+        
+        if i<len(distribution_parameters)-1:
+            axs[1].vlines(base_price, 0, gaussian(base_price), linestyles='dashed', color="black")
+        else:
+            axs[1].vlines(base_price, 0, gaussian(base_price), linestyles='dashed', color="black", label="diagnostic cost")
+        
+    axs[0].legend()
+    axs[1].legend()
+    plt.tight_layout()
+    plt.show()
+
+def nun_philipson(base_price, density_function, subsidy_vals=None, biased_update=False):
+    # automatically set a range for subsidies if not provided
+    if subsidy_vals is None:
+        subsidy_vals = np.linspace(0, base_price, 100)
+    
+    # prepare canvas for graphs
+    fig, axs = plt.subplots(2,2)
+  
+    # first graph is the plot of the total value for all possible subsidies
+    planner_values = []
+    for subsidy in subsidy_vals:
+        test_value, non_test_value, testing_frac = value_calculations(base_price-subsidy, density_function, biased_update=biased_update)
+        total_value = test_value + non_test_value - testing_frac*subsidy
+        planner_values.append(total_value)
+
+    # optimal subsidy calculations
+    max_index = np.argmax(np.array(planner_values))
+    axs[0,0].plot(subsidy_vals, planner_values)
+    axs[0,0].scatter(subsidy_vals[max_index], planner_values[max_index], color="red", label="optimal subsidy")
+    axs[0,0].set_xlabel("Subsidy per test ($)")
+    axs[0,0].set_ylabel("Patient value - planner costs ($)")
+    axs[0,0].legend()
+
+    # private value incentives
+    t_vals = np.linspace(0,1,1000)
+    net_testing_vals = [net_testing_value(t) for t in t_vals]
+    first_crossing, last_crossing = find_tradeoff_points(base_price)
+    subs_first_crossing, subs_last_crossing = find_tradeoff_points(base_price-subsidy_vals[max_index])
+
+    axs[0,1].plot(t_vals, net_testing_vals)
+    # unsubsidized
+    axs[0,1].hlines(base_price, 0, last_crossing+0.1, color="black")
+    axs[0,1].text(last_crossing+0.11, base_price, "Cost of testing", fontsize=8, verticalalignment='center')
+    axs[0,1].vlines(first_crossing, 0, base_price, color="black", linestyles="dashed")
+    axs[0,1].vlines(last_crossing, 0, base_price, color="black", linestyles="dashed")
+    #subsidized
+    axs[0,1].hlines(base_price-subsidy_vals[max_index], 0, subs_last_crossing, color="black")
+    axs[0,1].text(subs_last_crossing+0.01, base_price-subsidy_vals[max_index], "Cost subs. testing", fontsize=8, verticalalignment='center')
+    axs[0,1].vlines(subs_first_crossing, 0, base_price-subsidy_vals[max_index], color="black", linestyles="dashed")
+    axs[0,1].vlines(subs_last_crossing, 0, base_price-subsidy_vals[max_index], color="black", linestyles="dashed")
+
+    axs[0,1].set_ylim(0, max(net_testing_vals)+1)
+    axs[0,1].set_xlim(0,1)
+    axs[0,1].set_ylabel("Net benefit of testing ($)")
+    axs[0,1].set_xlabel("Patient risk for disease one")
+    subscript = "\u209B"
+    axs[0,1].set_xticks([0,first_crossing, last_crossing, subs_first_crossing, subs_last_crossing, 1], labels=[0,"L", "U", f"{"L"}{subscript}", f"{"U"}{subscript}", 1])
+    fig.text(0.7, 0.63, f"Privately chooses testing", wrap=True, horizontalalignment='center', fontsize=9)
+    curly_brace(axs[0,1], [first_crossing,0], [last_crossing,0], 1, 1, 0.1, 0.015, color="black")
+
+    fill_t_vals = np.linspace(first_crossing, last_crossing ,1000)
+    density_values = [density_function(t) for t in t_vals]
+    axs[1,1].plot(t_vals, density_values)
+    axs[1,1].set_xlim(0,1)
+    axs[1,1].set_xticks([0,first_crossing, last_crossing, subs_first_crossing, subs_last_crossing, 1], labels=[0,"L", "U", f"{"L"}{subscript}", f"{"U"}{subscript}", 1])
+    axs[1,1].set_xlabel("Patient risk for disease one")
+    axs[1,1].set_ylabel("Number of patients")
+    axs[1,1].set_yticks(())
+    # unsubsidized
+    axs[1,1].vlines(first_crossing, 0, density_function(first_crossing), color="black", linestyles="dashed")
+    axs[1,1].vlines(last_crossing, 0, density_function(last_crossing), color="black", linestyles="dashed")
+    axs[1,1].fill_between(fill_t_vals, [density_function(t) for t in fill_t_vals], alpha=0.3, color="blue")
+    midpoint = (last_crossing+first_crossing)/2
+    axs[1,1].text(midpoint, density_function(midpoint)/2, f"Population \ndemand for testing", horizontalalignment='center', fontsize=9)
+    # subsidized
+    subs_fill_t_vals = np.linspace(subs_first_crossing, subs_last_crossing, 1000)
+    axs[1,1].vlines(subs_first_crossing, 0, density_function(subs_first_crossing), color="black", linestyles="dashed")
+    axs[1,1].vlines(subs_last_crossing, 0, density_function(subs_last_crossing), color="black", linestyles="dashed")
+    axs[1,1].fill_between(subs_fill_t_vals, [density_function(t) for t in subs_fill_t_vals], alpha=0.3, color="blue")
+    axs[1,1].set_ylim(0,1.1*max(density_values))
+
+    test_value, non_test_value, testing_frac = value_calculations(base_price-subsidy_vals[max_index], density_function)
+    bar_locs = [-0.4, 0.0, 0.4]
+    
+    axs[1,0].set_xticks(bar_locs, labels=["Value to testers", "Value to non-testers", "Cost to planner"])
+    axs[1,0].set_ylabel("Value ($)")
+    axs[1,0].bar(bar_locs[0:-1], [test_value, non_test_value], width=0.2)
+    axs[1,0].bar([0.4], [subsidy_vals[max_index]*testing_frac], width=0.2, color="red")
+
+
     plt.tight_layout()
     plt.show()
 
@@ -264,15 +482,53 @@ def philipson_one_subsidy_experiment(prev_list, base_price=36, max_subsidy=None,
     plt.show()
 
 if __name__ == "__main__":
+    base_price = 20
+    def edge_cases(t, edge_width=0.1, edge_height=0.1):
+        # chosen so that the integral of this function on [0,1] is equal to 1
+        mid_height = (1-2*edge_width*edge_height)/(1-2*edge_width)
+
+        if t <= edge_width or t >= 1-edge_width:
+            return edge_height
+        else:
+            return mid_height
+
+    non_uniform = lambda t: edge_cases(t, edge_width=0.1, edge_height=0.1)
+    uniform = lambda t: 1
+    # sigma = 1/6
+    # non_uniform = lambda t: (1/sigma)/np.sqrt(2*np.pi)*np.exp(-(1/sigma)**2/2*(t-0.5)**2)
+    non_uniform = lambda t: 6*t*(1-t)
+
+    def triangular(t):
+        if 0<=t<=0.5:
+            return 4*t
+        else:
+            return 4*(1-t)
+
+    exported_parameters["cost_res"] *= 1.5
+    nun_philipson(base_price, non_uniform, biased_update=True)
+    nun_philipson(base_price, non_uniform)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     #other_changes = {"f_true": np.array([0.5, 0.5])}
-    other_changes=None
-    comparative_philipson("variance_res", [1,0.5,0.05], mode="*", max_subsidy=36, use_resampling=False, other_changes=other_changes)
-    comparative_philipson("variance_res", [1,0.5,0.05], mode="*", max_subsidy=36, use_resampling=False, other_changes=other_changes)
-    #comparative_philipson("cost_res", [0,5,10], max_subsidy=36, mode="+", other_changes=other_changes)
+    # other_changes=None
+    #bar_philipson(19, "variance_res", [0.5, 1, 2], mode="*")
+    # comparative_philipson("variance_res", [1,0.5,0.05], mode="*", max_subsidy=36, use_resampling=False, other_changes=other_changes)
+    #bar_philipson(21, "cost_res", [0,5,10], mode="+", other_changes=other_changes)
     #comparative_philipson("m", [4,8,16,32,64], max_subsidy=36)
     
-    prev_list = [np.array([0.9, 0.1]), np.array([0.8,0.2]), np.array([0.7,0.3]), np.array([0.6,0.4]), np.array([0.5,0.5])]
-    #comparative_philipson("f_0", prev_list, other_changes=other_changes, max_subsidy=36)
+    #prev_list = [np.array([0.9, 0.1]), np.array([0.8,0.2]), np.array([0.7,0.3]), np.array([0.6,0.4]), np.array([0.5,0.5])]
+    # prev_list = [np.array([0.9, 0.1])]
+    # bar_philipson(19, "f_true", prev_list)
 
     #comparative_philipson("cost_res", [1,1.25,1.5,1.75,3], mode="*")
     
